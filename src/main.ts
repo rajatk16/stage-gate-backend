9ed3d098dabf61c4f4f8742165dbd03bca0c0fe7
sudagar <rajat.sudagade@autodesk.com>
2025-08-24 11:56:13 +0530

    project setup

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import helmet from 'helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Middlewares
+  app.use(helmet());
+  app.enableCors();
+
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log(`StageGate API running on port ${port}`);
+}
+bootstrap().catch(console.error);
