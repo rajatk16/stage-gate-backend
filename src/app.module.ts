bd38e32d718a35cc8da06aa72e73e773e1e81138
sudagar <rajat.sudagade@autodesk.com>
2025-08-27 16:37:31 +0530

    Refactoring for SaaS

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,32 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { User, Talk, Review } from './entities';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: config.getOrThrow<string>('DB_HOST'),
-        port: +config.getOrThrow<number>('DB_PORT'),
-        username: config.getOrThrow<string>('DB_USER'),
-        password: config.getOrThrow<string>('DB_PASS'),
-        database: config.getOrThrow<string>('DB_NAME'),
-        entities: [User, Talk, Review],
+        url: config.getOrThrow<string>('DATABASE_URL'),
         autoLoadEntities: true,
         synchronize: true,
       }),
     }),
-    AuthModule,
-    UsersModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
--- src/dtos/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './LoginUser.dto';
-export * from './UpdateUser.dto';
-export * from './CreateUser.dto';
